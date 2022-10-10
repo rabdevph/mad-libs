0fcd33d5819e78076f7b7cb4833b2b1557367a3b
Ralph Arsy Bauto <rab.devph@gmail.com>
2022-10-10 19:46:13 +0400

    Re-write code and use different approach

--- madlibs.py
+++ madlibs.py
@@ -1,88 +1,49 @@
 import sys
+import re
 
 from logo import logo
-from text import text_libs
+from text import libs
+
+
+def replace_keyword(key_word, new_key_word, text):
+    """Find and replace keyword with new one."""
+    old_keyword = re.compile(fr'{key_word}')
+    new_text = old_keyword.sub(new_key_word.upper(), text, count=1)
+
+    return new_text
+
 
 print(logo['madlibs'])
 
-# Madlibs data. Keys are id that represents the number of each keyword to replace.
-# Value is dictionary containing details of keyword(such as the word itself) and
-# used for validation of user input.
-libs = {
-    1: {
-        'category': 'verb', 'word': '___', 'validation': str.isalpha
-    },
-    2: {
-        'category': 'verb', 'word': '___', 'validation': str.isalpha
-    },
-    3: {
-        'category': 'verb, ending with -ing', 'word': '___', 'validation': str.isalpha
-    },
-    4: {
-        'category': 'verb',  'word': '___', 'validation': str.isalpha
-    },
-    5: {
-        'category': 'plural noun', 'word': '___', 'validation': str.isalpha
-    },
-    6: {
-        'category': 'plural noun', 'word': '___', 'validation': str.isalpha
-    },
-    7: {
-        'category': 'verb, ending with -ing', 'word': '___', 'validation': str.isalpha
-    },
-    8: {
-        'category': 'plural color', 'word': '___', 'validation': str.isalpha
-    },
-    9: {
-        'category': 'plural color', 'word': '___', 'validation': str.isalpha
-    },
-    10: {
-        'category': 'number', 'word': '___', 'validation': str.isdigit
-    },
-    11: {
-        'category': 'verb', 'word': '___', 'validation': str.isalpha
-    },
-    12: {
-        'category': 'verb', 'word': '___', 'validation': str.isalpha
-    },
-    13: {
-        'category': 'verb', 'word': '___', 'validation': str.isalpha
-    },
-    14: {
-        'category': 'number', 'word': '___', 'validation': str.isdigit
-    },
-    15: {
-        'category': 'verb', 'word': '___', 'validation': str.isalpha
-    },
-    16: {
-        'category': 'adjective', 'word': '___', 'validation': str.isalpha
-    },
-}
+# Load content from other module
+content = libs['laundry']
 
-def run_madlibs():
-    """Run the game."""
-    print("    (Enter 'q' to quit.)")
-    
-    # Loop through id and details of the data.
-    for id, details in libs.items():
-        while True:
-            # Ask the user to enter a required keyword.
-            user_input = input(f"    {details['category'].capitalize()}: ")
-            # End the game when the user enter the 'q' character. 
-            if user_input == 'q':
-                sys.exit()
-            else:
-                # If user input is valid, save it with uppercase characters.q
-                if details['validation'](user_input):
-                    libs[id]['word'] = user_input.upper()
-                    break
-            # If user input is invalid, inform the user.
-            print('    Invalid input.')
+# Make a regular expression for the keywords
+find_keyword = re.compile(
+    r'VERB|GERUND VERB|PLURAL NOUN|PLURAL COLOR|NUMBER|ADJECTIVE')
 
-    print(logo['title'])
-    print(text_libs(libs))
+# Store all the matches in a list.
+found_keywords = find_keyword.findall(content)
 
-try:
-    run_madlibs()
-except KeyboardInterrupt:
-    sys.exit()
+# Loop through found keywords, then ask the user
+# for an input to replace the keywords.
+for i, keyword in enumerate(found_keywords):
+    while True:
+        new_keyword = input(f"{keyword}: ")
+        # Quit program when user input 'q' or 'Q'.
+        if new_keyword.upper() == 'Q':
+            sys.exit()
+        else:
+            # Check keyword for input validation(string or digit).
+            if (keyword in found_keywords) and (keyword != 'NUMBER') and (new_keyword.isalpha()):
+                content = replace_keyword(keyword, new_keyword, content)
+                break
+            elif (keyword in found_keywords) and (keyword == 'NUMBER') and (new_keyword.isdigit()):
+                content = replace_keyword(keyword, new_keyword, content)
+                break
+        # Input is invalid.
+        print('Invalid input.')
+
+
+print(logo['title'])
+print(content)
--- text.py
+++ text.py
@@ -1,17 +1,16 @@
-def text_libs(libs_dictionary):
+libs = {
+    'laundry': '''
+    In order to do your laundry, first you have to VERB your clothes.
+    Then you have to VERB the GERUND VERB machine.
+    Take your time not to VERB any items that may be gross.
+    Separate your PLURAL NOUN from your PLURAL NOUN and then load the GERUND VERB machine.
+    Make sure not to mix the PLURAL COLOR with the PLURAL COLOR.
+    Set temperature to NUMBER and begin the cycle.
+    After that is finished, VERB your clothes and VERB them in the dryer.
+    Set the temperature and VERB the dryer.
+    After about NUMBER minutes, it should be complete!
+    Now all you have to do is VERB your clothes and you are all set!
+    ADJECTIVE laundry!
+    ''',
+    }
     
-    text = f'''
-    In order to do your laundry, first you have to {libs_dictionary[1]['word']} your clothes.
-    Then you have to {libs_dictionary[2]['word']} the {libs_dictionary[3]['word']} machine.
-    Take your time not to {libs_dictionary[4]['word']} any items that may be gross.
-    Separate your {libs_dictionary[5]['word']} from your {libs_dictionary[6]['word']} and then load the {libs_dictionary[7]['word']} machine.
-    Make sure not to mix the {libs_dictionary[8]['word']} with the {libs_dictionary[9]['word']}.
-    Set temperature to {libs_dictionary[10]['word']} and begin the cycle.
-    After that is finished, {libs_dictionary[11]['word']} your clothes and {libs_dictionary[12]['word']} them in the dryer.
-    Set the temperature and {libs_dictionary[13]['word']} the dryer.
-    After about {libs_dictionary[14]['word']} minutes, it should be complete!
-    Now all you have to do is {libs_dictionary[15]['word']} your clothes and you are all set!
-    {libs_dictionary[16]['word']} laundry!
-    '''
-
-    return text
